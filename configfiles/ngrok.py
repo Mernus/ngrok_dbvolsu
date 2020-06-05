091b31da54d1de89293625b64c104895a6e3c664
Mernus <mernusv@gmail.com>
2020-06-05 19:25:17 +0400

    Settings update and cleanup

--- configfiles/ngrok.py
+++ configfiles/ngrok.py
@@ -1,21 +1,20 @@
-from log import set_log_settings
 import requests
-
+from log import set_log_settings
 from pyngrok import ngrok
+from settings import UPDATE_URL, NGROK_TOKEN
 
 set_log_settings()
-ngrok.set_auth_token("1co4FrKt6QbXd1V1Pf04xfKLqR4_7AsdkpmamHM1D23jJWhen")
+ngrok.set_auth_token(NGROK_TOKEN)
 
 url = ngrok.connect(5432, "tcp")
 ngrok_process = ngrok.get_ngrok_process()
 
 try:
     parsed_url = url[6:].split(':')
-    host, port = parsed_url
-    post_data = {'host': host, 'port': port}
-    print(post_data)
+    ngrok_host, ngrok_port = parsed_url
+    post_data = {'host': ngrok_host, 'port': ngrok_port}
 
-    r = requests.post("https://dbvolsu.herokuapp.com/def_update/", data=post_data)
+    r = requests.post(UPDATE_URL, data=post_data)
     ngrok_process.proc.wait()
 except KeyboardInterrupt:
     print("~~~Shutting down server~~~")
---
+++ configfiles/settings.py
@@ -0,0 +1,2 @@
+UPDATE_URL = "https://dbvolsu.herokuapp.com/def_update/"
+NGROK_TOKEN = "1co4FrKt6QbXd1V1Pf04xfKLqR4_7AsdkpmamHM1D23jJWhen"
